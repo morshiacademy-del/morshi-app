51f43c66726c4623824f611b0a9f20ba6a53b2b2
Sergio Florez <ferchoivan05@hotmail.com>
2025-10-25 20:34:27 -0500

    F008 - layout my courses screen

--- src/adapters/screens/home/MyCoursesScreen.tsx
+++ src/adapters/screens/home/MyCoursesScreen.tsx
@@ -1,15 +1,41 @@
 // Libraries
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 // Components
+import { ArrowRightIcon } from '@/adapters/assets/icons';
 import { AppBarComponent, ViewResponsive } from '@/adapters/components';
 
+// Constants - Utilities
+import { palette } from '@/adapters/constants';
+import { fontFamilyApp } from '@/adapters/utilities';
+
 export const MyCoursesScreen = () => {
+    const navigation = useNavigation();
+
     return (
         <ViewResponsive header={<AppBarComponent />}>
             <View style={styles.container}>
-                <Text>M I S - C U R S O S</Text>
+                <Text style={styles.titleSection}>Cursos inscritos</Text>
+                <FlatList
+                    data={Array.from({ length: 2 })}
+                    keyExtractor={(_, index) => index.toString()}
+                    contentContainerStyle={styles.containerFlatList}
+                    renderItem={({ index }) => (
+                        <TouchableOpacity
+                            style={styles.contentItem}
+                            activeOpacity={0.7}
+                            onPress={() => navigation.navigate('HomeStack', { screen: 'DetailsEnrolledCoursesScreen' })}
+                        >
+                            <View style={styles._contentInfoItem}>
+                                <Text style={styles._textTitleItem}>Curso {index + 1}</Text>
+                                <Text style={styles._textDescriptionItem}>Edward Vega • 12 lecciones</Text>
+                            </View>
+                            <ArrowRightIcon fill={palette.black_2} />
+                        </TouchableOpacity>
+                    )}
+                />
             </View>
         </ViewResponsive>
     );
@@ -18,7 +44,41 @@ export const MyCoursesScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        gap: 30,
+        paddingHorizontal: 24,
+        paddingVertical: 18,
+    },
+    titleSection: {
+        color: palette.black_2,
+        fontFamily: fontFamilyApp[700],
+        fontSize: 20,
+    },
+    containerFlatList: {
+        gap: 14,
+    },
+    contentItem: {
         alignItems: 'center',
+        backgroundColor: palette.gray_5,
+        borderRadius: 12,
+        flexDirection: 'row',
+        gap: 8,
+        paddingLeft: 14,
+        paddingRight: 20,
+        paddingVertical: 20,
+    },
+    _textTitleItem: {
+        color: palette.black_2,
+        fontFamily: fontFamilyApp[700],
+        fontSize: 20,
+    },
+    _textDescriptionItem: {
+        color: palette.gray_2,
+        fontFamily: fontFamilyApp[400],
+        fontSize: 14,
+    },
+    _contentInfoItem: {
+        flex: 1,
+        flexShrink: 1,
+        gap: 8,
     },
 });
